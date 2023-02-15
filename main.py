a165087597828c3713c598185982780d4f7e29dc
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-02-15 07:49:46 +0000

    test final

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ def find_mismatch(text):
             
             opening_brackets_stack.pop()
 
-    if not opening_brackets_stack:
-        
-        return "Success"
-    else:
+    if opening_brackets_stack:
         return opening_brackets_stack[0].position
+    return "Success"
 
 def main():
+    
+    ievietosana = input("Ievadi I vai F")
     text = input()
-    i = input()
+
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
 
